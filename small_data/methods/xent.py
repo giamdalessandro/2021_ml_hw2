1d9ec1d876f5f9ea8f42be2d861cf0beb90a21de
zascerta <giammarco.daless@gmail.com>
2021-12-13 15:21:18 +0100

    change optimizer template

--- small_data/methods/xent.py
+++ small_data/methods/xent.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 from typing import Callable
 
@@ -11,5 +12,45 @@ class CrossEntropyClassifier(BasicAugmentation):
     """
 
     def get_loss_function(self) -> Callable:
-
+        """ Returns a loss function.
+		
+		Returns
+		------
+		callable(*outputs, targets)
+			A loss function taking as arguments all model outputs followed by
+			target class labels and returning a single loss value.
+        """
         return nn.CrossEntropyLoss(reduction='mean')
+
+    def get_optimizer(self, model: nn.Module, max_epochs: int, max_iter: int) -> Tuple[torch.optim.Optimizer, torch.optim.lr_scheduler._LRScheduler]:
+        """ Instantiates an optimizer and learning rate schedule.
+
+        Parameters
+        ----------
+        model : nn.Module
+            The model to be trained.
+        max_epochs : int
+            The total number of epochs.
+        max_iter : int
+            The total number of iterations (epochs * batches_per_epoch).
+        
+        Returns
+        -------
+        optimizer : torch.optim.Optimizer
+        lr_schedule : torch.optim.lr_scheduler._LRScheduler
+        """
+        optimizer = torch.optim.Adam(model.parameters(), 
+			lr=self.hparams['lr'], 
+			# momentum=self.hparams['momentum'], 
+			weight_decay=self.hparams['weight_decay']
+		)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_iter)
+        return optimizer, scheduler
+        
+    @staticmethod
+    def default_hparams() -> dict:
+        hparams_dict = {
+            **super(CrossEntropyClassifier, CrossEntropyClassifier).default_hparams(),
+            'momentum' : 0.9
+        }
+        return hparams_dict

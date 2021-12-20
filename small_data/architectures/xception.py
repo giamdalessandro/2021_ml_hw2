4359f85385211156114d03d29bded8bcac127df1
zascerta <giammarco.daless@gmail.com>
2021-12-20 16:42:45 +0100

    end dropout

--- small_data/architectures/xception.py
+++ small_data/architectures/xception.py
@@ -182,7 +182,7 @@ class Xception(nn.Module):
         self.middle_rep  = middle_rep    # depth
         self.fc = nn.Sequential(         # fc top (optional)
             nn.Linear(256*widen_factor, num_classes),  
-            #nn.Dropout(0.5)
+            nn.Dropout(0.5)
         )
 
         for m in self.modules():
